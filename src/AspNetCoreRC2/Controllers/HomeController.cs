2982dc5a9cdb5c595d89d5867e526c20561bcff3
davidebbo <david.ebbo@microsoft.com>
2016-05-19 14:33:29 -0700

    Add some logging to Home controller

--- src/AspNetCoreRC2/Controllers/HomeController.cs
+++ src/AspNetCoreRC2/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyCoreLib;
 
 namespace AspNetCoreRC2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+
+        public HomeController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AccountController>();
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Some information log");
+            _logger.LogWarning("This is a warning");
+            _logger.LogError("This is an error");
+
             return View();
         }
 
